60b2c74b74dd3cc7ea9c1aaff459465f1142aa14
KhinPhone060 <60284009+KhinPhone060@users.noreply.github.com>
2024-11-14 19:10:58 +0630

    Initial Commit

---
+++ SEA/SEAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SEAApp.swift
+//  SEA
+//
+//  Created by Khin Phone Ei on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SEAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
